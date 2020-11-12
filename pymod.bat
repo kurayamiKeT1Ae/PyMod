3b37ab75fb77bd9dbb3430efb1faf9acb1c576c5
kurayami <kurayamiket1ae@gmail.com>
2020-11-13 01:03:27 +0200

    pymod V0.0.4

--- pymod.bat
+++
@@ -1,6 +0,0 @@
-
-
-@echo off
-
-py pymodrun.py %1
-
--- pymodinit.py
+++ pymodinit.py
@@ -1,20 +1,23 @@
 
 
 import os
+import sys
 
-VERSION = "0.0.2"
+VERSION = "0.0.4"
 
 CODE = """
-
 @echo off
-
 py pymodrun.py %1
-
 """
 
 def init():
-    with open("pymod.bat", 'w') as file:
+    filename = "pymod"
+    try:
+        filename = sys.argv[1]
+    except:
+        pass
+    with open(f"{filename}.bat", 'w') as file:
         file.writelines(CODE)
-
+    
 
 init()
